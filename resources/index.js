b90771e158f053e4914475349c0c621afc66b023
unknown <Brian Obanil>
2018-10-31 01:58:32 +0800

    initial commit

---
+++ resources/index.js
@@ -0,0 +1,144 @@
+var info = {
+  "items": {
+    "c001": {
+      "parent": "c000",
+      "title": "Phones"
+    },
+    "c002": {
+      "parent": "c000",
+      "title": "Laptops"
+    },
+    "c003": {
+      "parent": "c000",
+      "title": "Accessories"
+    },
+    "c004": {
+      "parent": "c100",
+      "title": "Cameras"
+    },
+    "p001": {
+      "parent": "c001",
+      "manufacturer": "Apple",
+      "title": "iPhone 10",
+      "price": 499,
+      "releaseDate": "2018-04-23T18:25:43.511Z"
+    },
+    "p002": {
+      "parent": "c002",
+      "manufacturer": "Apple",
+      "title": "Macbook Pro 2017",
+      "price": 1999,
+      "releaseDate": "2017-04-23T18:25:43.511Z"
+    },
+    "p003": {
+      "parent": "c002",
+      "manufacturer": "Dell",
+      "title": "XPS 13",
+      "price": 1399,
+      "releaseDate": "2017-04-23T18:25:43.511Z"
+    },
+    "p004": {
+      "parent": "c001",
+      "manufacturer": "Samsung",
+      "title": "Samsung Galaxy S10",
+      "price": 399,
+      "releaseDate": "2017-04-23T18:25:43.511Z"
+    },
+    "p005": {
+      "parent": "c003",
+      "manufacturer": "Apple",
+      "title": "Apple Charger 12W",
+      "price": 99,
+      "releaseDate": "2016-12-23T18:25:43.511Z"
+    },
+    "p006": {
+      "parent": "c003",
+      "manufacturer": "Samsung",
+      "title": "Samsung Charger 12W",
+      "price": 89,
+      "releaseDate": "2017-04-23T18:25:43.511Z"  
+    },
+    "p007": {
+      "parent": "c005",
+      "manufacturer": "Samsung",
+      "title": "Samsung VR Headset",
+      "price": 699,
+      "releaseDate": "2018-04-23T18:25:43.511Z"
+    }
+  }
+};
+
+// Gets all categories
+let get_categories = () => {
+  let category = []
+  Object.keys(info.items).forEach((key, index) => {
+    let object_content = info.items[key]
+    if(Object.keys(object_content).length == 2) {
+      object_content.code = key
+      category.push(
+        object_content
+      )
+    }
+  })
+  return category
+}
+
+// Selects all item in that category
+let pick_category = (category) => {
+  let products = []
+  $.each(info.items, (index, value) => {
+    let parent = value.parent
+    if(parent == category) {
+      products.push(value)
+    }
+  })
+  return products
+}
+
+// Loads categories into navbar
+let categories = get_categories()
+$.each(categories, (index, value) => {
+  $("#category").prepend(`<a href="#!" class="cat_selector" code="${ value.code }">${ value.title }</a>`)
+})
+
+// Removes active category
+const remove_active_category = () => {
+  $(".cat_selector").each(function() {
+    $(this).removeAttr("style")
+  })
+}
+
+// Changes content according to category selected
+$(".cat_selector").click(function() {
+  let category = $(this).attr("code")
+  remove_active_category()
+
+  $(this)
+    .css('color', 'rgb(51, 100, 156)')
+    .css('text-decoration', 'underline')
+
+  $("section").remove()
+
+  let current_items = pick_category(category)
+  $.each(current_items, (index, value) => {
+    let release_date = new Date(value.releaseDate)
+    $("article").append(`
+      <section>
+        <div class="content">
+          <h3>${ value.title }</h3>
+          <h4>${ value.manufacturer }</h4>
+          <div class="footer-content">PHP ${ value.price }</div>
+          <div class="footer-content text-right">Released: ${ release_date.getDate() }/${ release_date.getMonth() }/${ release_date.getFullYear() }</div>
+        </div>
+      </section>
+    `)
+  })
+  
+  if(current_items.length < 1) {
+    $("article").append(`
+      <section>
+        <h3>No product for this category.</h3>
+      </section>
+    `)
+  }
+})
